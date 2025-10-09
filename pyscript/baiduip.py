122548471c2057efe691fd1fedf340b6fbac3bce
szw <1639914395@qq.com>
2025-10-09 09:52:29 +0800

    百度获取ip归属地址修改

--- pyscript/baiduip.py
+++ pyscript/baiduip.py
@@ -2,12 +2,12 @@ import subprocess
 import json
 
 def getBaiDuip(ip):
-    cmd = "curl https://qifu-api.baidubce.com/ip/geo/v1/district?ip="+ip
+    cmd = f"curl  --location --request GET 'https://qifu.baidu.com/api/v1/ip-portrait/brief-info?ip={ip}' --header 'Referer: https://qifu.baidu.com/?_frm=aladdin&activeId=SEARCH_IP_ADDRESS&ip={ip}' "
     output = subprocess.check_output(cmd, shell=True)
     resp = output.decode("utf-8").strip()
     return resp
 resp = json.loads(getBaiDuip("{query}"))["data"]
-str = resp["prov"]+resp["city"]+resp["district"]
+str = resp["country"]+resp["province"]+resp["city"]+resp["isp"]
 items = [{
             'title': "{query}",
             'subtitle': str,
